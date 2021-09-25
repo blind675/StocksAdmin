4c36ddaabaddf9b7be744d18a7c9f2f171888f33
Catalin-Andrei BORA <catalin.bora@gmail.com>
2021-09-25 22:24:41 +0300

    CORS test

--- src/pages/ProblemsPage.tsx
+++ src/pages/ProblemsPage.tsx
@@ -14,7 +14,13 @@ const ProblemsPage: Component = () => {
           <div class="p-4 border-b border-solid border-gray-800 h-12 flex flex-row items-center justify-between">
             <strong class="w-14 text-gray-50">{problem.ticker}</strong>
             <p>{problem.message}</p>
-            <i class="fas fa-trash fill-current text-red-400 py-1"/>
+            <i class="fas fa-trash fill-current text-red-400 py-1"
+              onClick={async () => {
+                const response = await fetch(`https://stocktop100api.herokuapp.com/api/v1/admin/problems/${problem.id}`, {
+                  method: 'DELETE'
+                });
+                console.log(' -response: ', response);
+              }} />
           </div>
         }
       </For>
